61292325e106e412188384ed34d50bc56fed4837
Anna <annamtoman@gmail.com>
2023-04-19 13:41:30 -0500

    adds images data

---
+++ src/ProductsNew.jsx
@@ -0,0 +1,12 @@
+export function ProductsNew() {
+    return (
+        <div>
+            <h1>New Flowers:</h1>
+            <form>
+                <div>Name:<input name="name" type="text"/>
+                </div>
+
+            </form>
+        </div>
+    )
+}
