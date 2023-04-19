1209199d6cb030a8da4ab31d966582d9d7402449
Anna <annamtoman@gmail.com>
2023-04-19 15:04:14 -0500

    adds data

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,12 +1,29 @@
-export function ProductsNew() {
+export function ProductsNew(props) {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const params = new FormData(event.target);
+        props.onCreateProduct(params, () => event.target.reset());
+    };
+
     return (
         <div>
             <h1>New Flowers:</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <div>Supplier:<input name="supplier_id" type="text"/>
+                </div>
                 <div>Name:<input name="name" type="text"/>
                 </div>
-
+                <div>Price:<input name="price" type="text"/>
+                </div>
+                <div>Description:<input name="description" type="text"/>
+                </div>
+                <div>Image:<input name="image" type="text"/>
+                </div>
+                <div>Quantity in Stock:<input name="quantity_in_stock" type="text"/>
+                </div>
+                <button type="submit">Submit</button>
             </form>
         </div>
-    )
+    );
 }
