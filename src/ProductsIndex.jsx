9467ba09ea713cd041ff8eaa7e09dde2f0b44b02
Anna <annamtoman@gmail.com>
2023-04-19 12:56:09 -0500

    adds axios web request

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,14 @@
-export function ProductsIndex() {
+export function ProductsIndex(props) {
     return (
         <div>
             <h1>Hello!</h1>
+            {props.products.map((products) => (
+            <div key={products.id}>
+            <h1>{products.name}</h1>
+            <h2>{products.price}</h2>
+            <h2>{products.description}</h2>
+            </div>
+            ))}
         </div>
     );
 }
