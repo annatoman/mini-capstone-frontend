3fefad7d26e86d35a18b55ea3b6e6b8f88ef1f28
Anna <annamtoman@gmail.com>
2023-04-19 12:58:05 -0500

    adds signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
+import { Signup } from "./Signup";
 import { Footer } from "./Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Signup />
       <Content />
       <Footer />
     </div>
